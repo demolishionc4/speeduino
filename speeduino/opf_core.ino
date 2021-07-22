b2ac6f4d53235af37586e32c8ede032fe77ac2de
Benas Brazdziunas <b.brazdziunas@gmail.com>
2021-07-22 16:33:05 +0100

    update to core board

--- speeduino/opf_core.ino
+++ speeduino/opf_core.ino
@@ -3,8 +3,41 @@
 
 #include "opf_core.h"
 
+//unsigned boot_count __attribute__((__section__(".noinit")));
+
 void setupBoard()
 {
+  
+  #define PIN_SERIAL_RX        PA10
+  #define PIN_SERIAL_TX        PA9
+  #define PIN_SERIAL2_RX        PA10
+  #define PIN_SERIAL2_TX        PA9
+  #define PIN_SERIAL3_RX        PB10
+  #define PIN_SERIAL3_TX        PB11
+
+  pinMode(PB12, OUTPUT);
+  pinMode(PB13, OUTPUT);
+  pinMode(PB14, OUTPUT);
+  pinMode(PB15, OUTPUT);
+
+
+#if defined(USE_SPI_EEPROM)
+
+
+  SPIClass SPI_for_flash(113, 112, 111); //SPI1_MOSI, SPI1_MISO, SPI1_SCK
+
+  SPI_for_flash.setMOSI(113);
+  SPI_for_flash.setMISO(112);
+  SPI_for_flash.setSCLK(111);
+  SPI_for_flash.setSSEL(110);
+
+  EEPROM_Emulation_Config EmulatedEEPROMMconfig{255UL, 4096UL, 31, 0x00100000UL};
+  Flash_SPI_Config SPIconfig{USE_SPI_EEPROM, SPI_for_flash};
+  SPI_EEPROM_Class EEPROM(EmulatedEEPROMMconfig, SPIconfig);
+  
+  #define pinIsReserved(pin) (((pin) == PA11) || ((pin) == PA12) || ((pin) == USE_SPI_EEPROM))
+#endif
+
 }
 
 void initialiseBoard()
@@ -20,33 +53,19 @@ void initialiseBoard()
   pinMode(PG12, OUTPUT);
 
   //SPI FLASH
-
-#if defined(USE_SPI_EEPROM)
-
-  pinMode(PB12, OUTPUT);
-  pinMode(PB13, OUTPUT);
-  pinMode(PB14, OUTPUT);
-  pinMode(PB15, OUTPUT);
-  SPIClass SPI_for_flash(113, 112, 111); //SPI1_MOSI, SPI1_MISO, SPI1_SCK
-
-  SPI_for_flash.setMOSI(113);
-  SPI_for_flash.setMISO(112);
-  SPI_for_flash.setSCLK(111);
-  SPI_for_flash.setSSEL(110);
-
-  EEPROM_Emulation_Config EmulatedEEPROMMconfig{255UL, 4096UL, 31, 0x00100000UL};
-  Flash_SPI_Config SPIconfig{USE_SPI_EEPROM, SPI_for_flash};
-  SPI_EEPROM_Class EEPROM(EmulatedEEPROMMconfig, SPIconfig);
-#endif
-
-#define pinIsReserved(pin) (((pin) == PA11) || ((pin) == PA12) || ((pin) == USE_SPI_EEPROM))
 }
 
 void runLoop()
 {
+  if ( (Serial.available()) > 0){
+    digitalToggle(PG12);
+  }
+
   if (BIT_CHECK(LOOP_TIMER, BIT_TIMER_1HZ)) //1 hertz
   {
     digitalToggle(PG9);
+    
+    pinMode(PB12, OUTPUT);
   }
   if (BIT_CHECK(LOOP_TIMER, BIT_TIMER_4HZ)) //4 hertz
   {
@@ -58,10 +77,14 @@ void runLoop()
   }
   if (BIT_CHECK(LOOP_TIMER, BIT_TIMER_15HZ)) //15 hertz
   {
-    digitalToggle(PG12);
   }
   if (BIT_CHECK(LOOP_TIMER, BIT_TIMER_30HZ)) //30 hertz
   {
+    
+    digitalToggle(PB12);
+    digitalToggle(PB13);
+    digitalToggle(PB14);    
+    digitalToggle(PB15);
   }
 }
 
